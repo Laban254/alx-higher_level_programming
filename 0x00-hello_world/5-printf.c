314fdd3f0cc41cdd167893934d616557e74fc430
laban254 <labanrotich6544@gmail.com>
2022-09-08 13:20:58 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main - entry point
  * printf "with proper grammar, but the outcome is a piece of art,"
- * return 0
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art," );
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
