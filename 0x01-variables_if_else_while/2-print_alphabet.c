bb45daebe561e7f59a8112170ce3853343d2200c
laban254 <labanrotich6544@gmail.com>
2022-09-09 12:42:59 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description  prints alphabets in lowercase
+ * Return: o
+ */
+
+int main(void)
+{
+	int cha;
+
+	for (cha => a, cha < z, cha++)
+	{
+		putchar(cha\t);
+	}
+	return (0);
+}
