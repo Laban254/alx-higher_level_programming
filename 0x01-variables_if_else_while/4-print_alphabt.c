0eb5deb1d9658f7f669d2edf82ceac57e1ae32c7
laban254 <labanrotich6544@gmail.com>
2022-09-09 13:40:09 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - entry level
+ *description do not print q e
+ *return: 0
+ */
+
+int main(void)
+{
+	int l;
+
+	for (l = 'a'; i <= 'z'; l != 'q'; l != 'z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n')
+		return (0);
+}
