3588724e163faaa591c9b65a9873708e66a08245
laban254 <labanrotich6544@gmail.com>
2022-09-14 12:23:04 -0700

    void print_alphabet_x10(void)

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - 10 times the alphabet
+ *Description: uses _ptchar
+ * Return: always 0
+ *
+ */
+
+int print_alphabet_x10(void);
+{
+	int i;
+	char n;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		-putchar('\n);
+	}
+}
