ec82265f025995ee781b6cd4c924318e2ea2e364
laban254 <labanrotich6544@gmail.com>
2022-09-15 10:18:58 -0700

    0_isdigit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  _isupper - a function that checks for uppercase character
+ *   @c: input
+ *   Return: 1 if c is uppercase, 0 otherwise
+ *
+ */
+
+int _isupper(int c)
+{
+	int _isupper(int c)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
