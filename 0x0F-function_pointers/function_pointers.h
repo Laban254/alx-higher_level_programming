b405a1b3bdef6d22d9a7d8eca0503fcad90b1d36
laban254 <labanrotich6544@gmail.com>
2022-10-13 07:45:23 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif /* FUNCTION_POINTERS_H */
