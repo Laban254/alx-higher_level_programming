64cda2bfb43cb6c45964385ac07e4e9847383803
laban254 <labanrotich6544@gmail.com>
2022-12-05 10:18:11 -0800

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+ble File  10 lines (9 sloc)  284 Bytes
+
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for column in row:
+            if column == row[-1]:
+                print('{:d}'.format(column), end='')
+            else:
+                print('{:d}'.format(column), end=' ')
+        print()
