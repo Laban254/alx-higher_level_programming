457760115e8beb128fcafd0f7bab8b16a3b4f65f
laban254 <labanrotich6544@gmail.com>
2023-01-09 08:03:26 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,8 +1,9 @@
 #!/usr/bin/pyhon3
 
-''' Returns a list of class attributes'''
+
+""" Returns a list of class attributes"""
+
 
 def lookup(obj):
-    '''returns the list of available attributes and methods of an object'''
-
+    """returns the list of available attributes and methods of an object"""
     return (dir(obj))
